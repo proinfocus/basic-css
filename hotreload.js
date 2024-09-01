4df2f58f7d6643a25ce437d73c25e29450d5a494
Proinfocus <rahul@proinfocus.com>
2024-09-01 19:45:18 +0530

    Update hotreload.js

--- hotreload.js
+++ hotreload.js
@@ -1,7 +1,9 @@
-let socket = new WebSocket("ws://localhost:9000/");
-socket.onopen = e => console.log('WebSocket connection established');
-socket.onmessage = e => {
-    setTimeout(() => {
-        location.reload();
-    }, 1000);
+if (location.href.indexOf("//localhost:") >= 0) {
+    let socket = new WebSocket("ws://localhost:9000/");
+    socket.onopen = e => console.log('WebSocket connection established');
+    socket.onmessage = e => {
+        setTimeout(() => {
+            location.reload();
+        }, 1000);
+    }
 }
